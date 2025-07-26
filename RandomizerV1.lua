8e122f83c18647204c783c6191543a82a45c3c36
ZenkiHub <visamindaaventurado4@gmail.com>
2025-07-26 19:10:58 +0800

    Add RandomizerV1.lua

---
+++ RandomizerV1.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ld8U6n85nwrHnLWK/raw", true))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenkiHub/Keyless/main/PetRandomizerXMutation.lua"))()
